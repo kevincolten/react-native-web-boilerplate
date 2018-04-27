0ca6ac3b75998d35a787dc15401e6f25659dba55
Kevin Colten <kevincolten@gmail.com>
2018-04-27 11:15:39 -0500

    DB_DIALECT

--- config/database/config.js
+++ config/database/config.js
@@ -8,6 +8,6 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
+    dialect: process.env.DB_DIALECT
   },
 }
